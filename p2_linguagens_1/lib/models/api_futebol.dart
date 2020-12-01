58954b13ad6e7ef61f324055bdec8b37598d9c18
JoseGMS <56358974+JoseGMS@users.noreply.github.com>
2020-11-30 23:32:32 -0300

    inicio da API e criação futebolmodel

--- p2_linguagens_1/lib/models/api_futebol.dart
+++ p2_linguagens_1/lib/models/api_futebol.dart
@@ -1,1 +1,13 @@
 import 'package:http/http.dart' as http;
+
+class FutAPI {
+
+  final String apiUrl = "https://api.api-futebol.com.br/v1/";
+
+  static const api_key = "live_00c695e09e2aef1f69c601024afd49";
+
+  static const headers = {
+    'Authorization: Bearer' api_key
+  };
+
+}
